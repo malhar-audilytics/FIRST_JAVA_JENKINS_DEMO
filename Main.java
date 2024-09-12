89d2cdc4f84d01a08b8d1f11c3b2b39a1858960d
malhar-audilytics <malhar.k@audilyticssolutions.com>
2024-09-12 08:25:33 +0530

    Initial Commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello from java");
+        System.out.println("This is first demo java program to work with jenkins");
+        System.out.println("Happy Learning!!!!!");
+    }
+}
